519f8e56a74282149732d36e0b62dc3f706b2ae3
Magusest <redikor49@gmail.com>
2023-03-16 00:14:52 +0300

    Fix error when delete about block

--- source/js/modules/my-modules/about.js
+++ source/js/modules/my-modules/about.js
@@ -2,17 +2,19 @@ function showText() {
   const buttonAbout = document.querySelector('[data-button-about]');
   const hiddenTexts = document.querySelectorAll('[data-hidden-text]');
 
-  buttonAbout.addEventListener('click', () => {
-    hiddenTexts.forEach((text) => {
-      if (text.style.display === 'inline') {
-        text.style.display = 'none';
-        buttonAbout.textContent = 'подробнее';
-      } else {
-        text.style.display = 'inline';
-        buttonAbout.textContent = 'Свернуть';
-      }
+  if (buttonAbout) {
+    buttonAbout.addEventListener('click', () => {
+      hiddenTexts.forEach((text) => {
+        if (text.style.display === 'inline') {
+          text.style.display = 'none';
+          buttonAbout.textContent = 'подробнее';
+        } else {
+          text.style.display = 'inline';
+          buttonAbout.textContent = 'Свернуть';
+        }
+      });
     });
-  });
+  }
 }
 
 export {
