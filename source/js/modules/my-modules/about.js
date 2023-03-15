9ac99a74a1e7dba57dc03f8108f15c0a71693287
Magusest <redikor49@gmail.com>
2023-03-15 10:19:55 +0300

    Fix bugs about

--- source/js/modules/my-modules/about.js
+++ source/js/modules/my-modules/about.js
@@ -2,19 +2,17 @@ function showText() {
   const buttonAbout = document.querySelector('[data-button-about]');
   const hiddenTexts = document.querySelectorAll('[data-hidden-text]');
 
-  if (buttonAbout) {
-    buttonAbout.addEventListener('click', () => {
-      hiddenTexts.forEach((text) => {
-        if (text.style.display === 'inline') {
-          text.style.display = 'none';
-          buttonAbout.textContent = 'подробнее';
-        } else {
-          text.style.display = 'inline';
-          buttonAbout.textContent = 'Свернуть';
-        }
-      });
+  buttonAbout.addEventListener('click', () => {
+    hiddenTexts.forEach((text) => {
+      if (text.style.display === 'inline') {
+        text.style.display = 'none';
+        buttonAbout.textContent = 'подробнее';
+      } else {
+        text.style.display = 'inline';
+        buttonAbout.textContent = 'Свернуть';
+      }
     });
-  }
+  });
 }
 
 export {
