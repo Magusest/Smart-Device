0f07174c346a630ffc9ce2279b9159882fcfe696
Magusest <redikor49@gmail.com>
2023-03-20 02:01:59 +0300

    Fix bugs

--- source/js/modules/my-modules/pop-up.js
+++ source/js/modules/my-modules/pop-up.js
@@ -1,23 +1,39 @@
 const popUpOpenButton = document.querySelector('[data-popup-button]');
 const popUpCloseButton = document.querySelector('[data-popup-button="close"]');
 const popUp = document.querySelector('[data-popup]');
+const popUpModal = document.querySelector('.pop-up__wrapper');
 
 
 function showPopUp() {
+
+  const hidePopUp = () => {
+    popUp.style.opacity = '0';
+    popUp.style.zIndex = '-10';
+    document.body.style.overflow = '';
+  };
+
   const closeButtonHendler = () => {
     popUpCloseButton.addEventListener('click', () => {
-      popUp.style.opacity = '0';
-      popUp.style.zIndex = '-10';
+      hidePopUp();
     });
+  };
+
+  const onDocumentKeyDown = (evt) => {
+    if (evt.key === 'Escape') {
+      evt.preventDefault();
+      hidePopUp();
+    }
   };
 
   const openButtonHendler = () => {
     popUp.style.opacity = '1';
     popUp.style.zIndex = '10';
+    document.body.style.overflow = 'hidden';
     closeButtonHendler();
   };
 
   popUpOpenButton.addEventListener('click', openButtonHendler);
+  document.addEventListener('keydown', onDocumentKeyDown);
 }
 
 export {
