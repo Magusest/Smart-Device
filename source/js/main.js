dcfe4b9c6b988e114973d0fc10ba12e9aadd9098
Magusest <redikor49@gmail.com>
2023-03-19 23:51:12 +0300

    Fix overwritting blocks

--- source/js/main.js
+++ source/js/main.js
@@ -1,6 +1,4 @@
 import {iosVhFix} from './utils/ios-vh-fix';
-import {initModals} from './modules/modals/init-modals';
-import {Form} from './modules/form-validate/form';
 import {showText} from './modules/my-modules/about';
 import {showMenu} from './modules/my-modules/menu';
 import {showPopUp} from './modules/my-modules/pop-up';
@@ -25,12 +23,6 @@ window.addEventListener('DOMContentLoaded', () => {
 
   // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
   // в load следует добавить скрипты, не участвующие в работе первого экрана
-  window.addEventListener('load', () => {
-    initModals();
-    const form = new Form();
-    window.form = form;
-    form.init();
-  });
 });
 
 // ---------------------------------
