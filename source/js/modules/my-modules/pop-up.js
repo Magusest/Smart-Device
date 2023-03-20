80c5bd5bf7d92330c6daf0f12b2c9dbd0d5be29c
Magusest <redikor49@gmail.com>
2023-03-20 11:20:44 +0300

    Add new listener

--- source/js/modules/my-modules/pop-up.js
+++ source/js/modules/my-modules/pop-up.js
@@ -1,7 +1,6 @@
 const popUpOpenButton = document.querySelector('[data-popup-button]');
 const popUpCloseButton = document.querySelector('[data-popup-button="close"]');
 const popUp = document.querySelector('[data-popup]');
-const popUpModal = document.querySelector('.pop-up__wrapper');
 
 
 function showPopUp() {
@@ -29,16 +28,16 @@ function showPopUp() {
     popUp.style.opacity = '1';
     popUp.style.zIndex = '10';
     document.body.style.overflow = 'hidden';
+    popUp.addEventListener('click', (e)=> {
+      if (e.target === popUp) {
+        hidePopUp();
+      }
+    });
     closeButtonHendler();
   };
 
   popUpOpenButton.addEventListener('click', openButtonHendler);
   document.addEventListener('keydown', onDocumentKeyDown);
-  window.addEventListener('click', (evt) => {
-    if (!evt.target(popUpModal)) {
-      hidePopUp();
-    }
-  });
 }
 
 export {
